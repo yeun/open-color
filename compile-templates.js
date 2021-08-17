c1986061391584cf7337d1b6791ffd696ccedd47
Surjith S M <surjithctly@gmail.com>
2021-08-17 16:58:59 +0530

    revert formatting

--- compile-templates.js
+++ compile-templates.js
@@ -1,3 +1,4 @@
+
 const path = require('path');
 const pkg = require('./package.json');
 
@@ -12,106 +13,49 @@ const colors = require(COLORS_FILE);
 
 const templatedBuilder = new TemplatedBuilder(colors, pkg.version);
 const aseBuilder = new AseBuilder(colors, pkg.version);
-const craftLibraryBuilder = new CraftLibraryBuilder(
-  colors,
-  pkg.version
-);
+const craftLibraryBuilder = new CraftLibraryBuilder(colors, pkg.version);
 const gvswatchBuilder = new GVSwatchBuilder(colors, pkg.version);
 
-templatedBuilder.build('_config.yml', [
-  path.join(__dirname, 'docs', '_config.yml'),
-]);
-templatedBuilder.build('open-color.scss', [
-  path.join(__dirname, 'open-color.scss'),
-  path.join(__dirname, 'docs', '_sass', '_open-color.scss'),
-]);
-templatedBuilder.build('open-color.css', [
-  path.join(__dirname, 'open-color.css'),
-]);
-templatedBuilder.build('open-color.js', [
-  path.join(__dirname, 'open-color.js'),
-]);
-templatedBuilder.build('open-color.less', [
-  path.join(__dirname, 'open-color.less'),
-]);
-templatedBuilder.build('open-color.styl', [
-  path.join(__dirname, 'open-color.styl'),
-]);
-templatedBuilder.build('open-color.svg', [
-  path.join(__dirname, 'docs', 'asset', 'images', 'open-color.svg'),
-]);
-templatedBuilder.build('open-color.tex', [
-  path.join(__dirname, 'open-color.tex'),
-]);
+templatedBuilder.build('_config.yml',
+    [path.join(__dirname, 'docs', '_config.yml')]);
+templatedBuilder.build('open-color.scss',
+    [path.join(__dirname, 'open-color.scss'), path.join(__dirname, 'docs', '_sass', '_open-color.scss')]);
+templatedBuilder.build('open-color.css',
+    [path.join(__dirname, 'open-color.css')]);
+templatedBuilder.build('open-color.js',
+    [path.join(__dirname, 'open-color.js')]);
+templatedBuilder.build('open-color.less',
+    [path.join(__dirname, 'open-color.less')]);
+templatedBuilder.build('open-color.styl',
+    [path.join(__dirname, 'open-color.styl')]);
+templatedBuilder.build('open-color.svg',
+    [path.join(__dirname, 'docs', 'asset', 'images', 'open-color.svg')]);
+templatedBuilder.build('open-color.tex',
+    [path.join(__dirname, 'open-color.tex')]);
 
-templatedBuilder.build('open-color.sketchpalette', [
-  path.join(
-    __dirname,
-    'docs',
-    'asset',
-    'download',
-    `open-color_${pkg.version}.sketchpalette`
-  ),
-]);
+templatedBuilder.build('open-color.sketchpalette',
+    [path.join(__dirname, 'docs', 'asset', 'download', `open-color_${pkg.version}.sketchpalette`)]);
 
-templatedBuilder.build('open-color.inkscape', [
-  path.join(
-    __dirname,
-    'docs',
-    'asset',
-    'download',
-    `open-color_${pkg.version}.gpl`
-  ),
-]);
+templatedBuilder.build('open-color.inkscape',
+    [path.join(__dirname, 'docs', 'asset', 'download', `open-color_${pkg.version}.gpl`)]);
 
-templatedBuilder.build('open-color.code-snippets', [
-  path.join(
-    __dirname,
-    'docs',
-    'asset',
-    'download',
-    `open-color_${pkg.version}.code-snippets`
-  ),
-]);
+templatedBuilder.build('open-color.code-snippets',
+    [path.join(__dirname, 'docs', 'asset', 'download', `open-color_${pkg.version}.code-snippets`)]);
 
-aseBuilder.build('ase', [
-  path.join(
-    __dirname,
-    'docs',
-    'asset',
-    'download',
-    `open-color_${pkg.version}.ase`
-  ),
-]);
+aseBuilder.build('ase',
+    [path.join(__dirname, 'docs', 'asset', 'download', `open-color_${pkg.version}.ase`)]);
 
-craftLibraryBuilder.build([
-  path.join(
-    __dirname,
-    'docs',
-    'asset',
-    'download',
-    `open-color_${pkg.version}.library`
-  ),
-]);
+craftLibraryBuilder.build(
+    [path.join(__dirname, 'docs', 'asset', 'download', `open-color_${pkg.version}.library`)]);
 
-gvswatchBuilder.build([
-  path.join(
-    __dirname,
-    'docs',
-    'asset',
-    'download',
-    `open-color_${pkg.version}.gvswatch`
-  ),
-]);
+gvswatchBuilder.build(
+    [path.join(__dirname, 'docs', 'asset', 'download', `open-color_${pkg.version}.gvswatch`)])
 
-templatedBuilder.build('open-color.oco', [
-  path.join(__dirname, 'open-color.oco'),
-]);
+templatedBuilder.build('open-color.oco',
+    [path.join(__dirname, 'open-color.oco')]);
 
-templatedBuilder.build('open-color.rcpx', [
-  path.join(__dirname, 'open-color.rcpx'),
-]);
+templatedBuilder.build('open-color.rcpx',
+    [path.join(__dirname, 'open-color.rcpx')]);
 
-templatedBuilder.build('open-color.d.ts', [
-  path.join(__dirname, 'open-color.d.ts'),
-]);
+templatedBuilder.build('open-color.d.ts',
+    [path.join(__dirname, 'open-color.d.ts')]);
